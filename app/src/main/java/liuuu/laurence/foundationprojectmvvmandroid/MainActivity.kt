7990b02722aef9369b2f949dc1b3ee4f43d2723b
laurence-liu <laurence.liuuu@gmail.com>
2019-08-15 16:03:29 -0700

    First commit

---
+++ app/src/main/java/liuuu/laurence/foundationprojectmvvmandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package liuuu.laurence.foundationprojectmvvmandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
